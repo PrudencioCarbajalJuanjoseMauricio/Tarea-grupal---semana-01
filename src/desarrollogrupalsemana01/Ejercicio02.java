1e5b993d41d734f5852e8c40bc32abb41459dd44
VentocillaCarbajalOmar <>
2025-08-16 20:32:32 -0500

    EJERCICIO 02 (FORMULA MATEMATICA)

--- src/desarrollogrupalsemana01/Ejercicio02.java
+++ src/desarrollogrupalsemana01/Ejercicio02.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package desarrollogrupalsemana01;
 
-/**
- *
- * @author bryan
- */
+import java.util.Scanner;
 public class Ejercicio02 {
-    
+     public static void main(String[] args) {
+         
+        Scanner entrada = new Scanner(System.in);
+        
+        
+        double a, b, c;   
+        int rpt;          
+        
+        
+        System.out.print("Ingrese el valor de a: ");
+        a = entrada.nextDouble();
+        
+        System.out.print("Ingrese el valor de b: ");
+        b = entrada.nextDouble();
+        
+        System.out.print("Ingrese el valor de c: ");
+        c = entrada.nextDouble();
+        
+       
+        rpt = (int)((a * a + b * b) / c);
+        
+       
+        System.out.println("El resultado de la fórmula es: " + rpt); 
+         
+     }
 }
