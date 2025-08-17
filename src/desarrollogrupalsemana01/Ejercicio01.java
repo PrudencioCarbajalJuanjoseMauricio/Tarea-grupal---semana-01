3ceb45e428dcf95934abedcbecdad3e2ec4e4033
Osorio Angeles Bryan <>
2025-08-16 20:04:25 -0500

    En este problema realize un seudocodigo para hallar el area de un cuadrado para esto utilize el scanner lector para que el usuario pueda ingresar los datos para de esta manera hallar el area de un cuadrado.

--- src/desarrollogrupalsemana01/Ejercicio01.java
+++ src/desarrollogrupalsemana01/Ejercicio01.java
@@ -8,6 +8,18 @@ package desarrollogrupalsemana01;
  *
  * @author USUARIO
  */
+import java.util.Scanner;
 public class Ejercicio01 {
-    
+    public static void main(String[] args) {
+      double area,lado;
+      Scanner lector=new Scanner(System.in);
+
+        System.out.println("Ingrese el area del cuadrado");
+        lado=lector.nextDouble();
+
+        area=(lado*lado);
+         System.out.println("El resultado es "+area);
+    }
+
 }
+
